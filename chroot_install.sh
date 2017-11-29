2db7966a4ee4bc4e1bb06f88d9dbc37f7b40c7b6
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-11-29 19:35:25 +0200

    Minor

--- chroot_install.sh
+++ chroot_install.sh
@@ -16,11 +16,14 @@ locale-gen
 echo LANG=$LANG > /etc/locale.conf
 
 echo $HOSTNAME > /etc/hostname
-echo "127.0.1.1    $HOSTNAME.localdomain    $HOSTNAME" >> /etc/hosts
-systemctl enable dhcpcd.service
 
 pacman -S --noconfirm grub
 grub-install --target=i386-pc --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "root:$ROOT_PASSWORD" | chpasswd
+
+pacman -S --noconfirm intel-ucode
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable dhcpcd.service
--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
+timedatectl set-ntp true
+
 parted /dev/sda -s "mklabel msdos"
 parted /dev/sda -s "mkpart primary ext4 0% 100%"
 parted /dev/sda -s "set 1 boot on"
-
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
 
-
-timedatectl set-ntp true
 pacstrap /mnt base
-#Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
 cp chroot_install.sh /mnt/root
@@ -20,4 +18,3 @@ rm -f /mnt/root/chroot_install.sh
 umount -R /mnt
 
 echo "Installation completed. Please reboot (systemctl reboot)"
-
