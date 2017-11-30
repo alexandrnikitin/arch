1cf8785fe5acca6670c0ad69e1228a9718b539f1
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-11-30 12:34:32 +0200

    Minor

--- bootstrap/install_base.sh
+++ bootstrap/install_base.sh
@@ -6,15 +6,21 @@ parted /dev/sda -s "mklabel msdos"
 parted /dev/sda -s "mkpart primary ext4 0% 100%"
 parted /dev/sda -s "set 1 boot on"
 mkfs.ext4 /dev/sda1
+
 mount /dev/sda1 /mnt
 
 pacstrap /mnt base
 genfstab -U /mnt >> /mnt/etc/fstab
+#sed -r -e 's#rw,relatime,data=ordered#defaults,noatime,discard#g' -i /mnt/etc/fstab
 
 cp chroot_install.sh /mnt/root
 arch-chroot /mnt /root/chroot_install.sh
-
 rm -f /mnt/root/chroot_install.sh
+
 umount -R /mnt
 
-echo "Installation completed. Please reboot (systemctl reboot)"
+echo ""
+echo ""
+echo "Installation completed, you can reboot safely now. (systemctl reboot)"
+echo ""
+echo ""
