0e0b750ac51ea5720e300fc6a60bc27ae315e12b
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-12-04 20:09:43 +0200

    Install python

--- bootstrap/install_chroot.sh
+++ bootstrap/install_chroot.sh
@@ -31,7 +31,7 @@ mkswap /swapfile
 echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 echo "vm.swappiness=10" >> /etc/sysctl.d/99-sysctl.conf
 
-pacman -S --noconfirm base-devel
+pacman -S --noconfirm base-devel python
 pacman -S --noconfirm xorg xf86-video-vesa xf86-input-mouse xf86-input-keyboard
 pacman -S --noconfirm openssh virtualbox-guest-modules-arch
 
